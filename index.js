afe99c1063482dc497f748b9ea1be7163de4f2e4
Richa Singh <121396620+Richasi@users.noreply.github.com>
2024-01-14 19:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const cors = require("cors");
+
 
 const { connection } = require("./config/db");
 
@@ -13,7 +13,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
- app.use(cors());
+
 
 app.use("/users", userRouter);
  app.use("/posts", auth, postRouter);
