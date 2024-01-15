8b2f21a7b688e92417cdc6000dcf567a52c10704
Richa Singh <121396620+Richasi@users.noreply.github.com>
2024-01-15 17:00:28 +0530

    Update user.model.js

--- model/user.model.js
+++ model/user.model.js
@@ -3,24 +3,6 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
 
-const mongoose = require("mongoose");
-
-const userSchema = mongoose.Schema({
-
-name: {type:String,required:true},
-
-email: {type:String,required:true,unique:true},
-
-gender:{type:String,required:true,enum:["Male","Female","Others"]},
-
-password:{type:String,required:true},
-age : {type:Number,required:true},
-city : {type:String,required:true},
-
-});
-
-const UserModel = mongoose.model("user", userSchema); 
-module.exports = { UserModel };
 name: String,
 
 email: String,
